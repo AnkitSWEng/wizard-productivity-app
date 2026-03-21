340cde3cb980a2d51c0422e112e454f53cfce9bd
Ankit Kumar <ankitsweng@gmail.com>
2026-03-21 10:13:09 +0530

    Initial commit

---
+++ src/models/Task.js
@@ -0,0 +1,13 @@
+// /src/models/Task.js
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  userId: String,
+  title: String,
+  totalTimeSeconds: { type: Number, default: 0 },
+  isActive: { type: Boolean, default: false },
+  startedAt: { type: Number, default: null },
+  date: String
+}, { timestamps: true });
+
+export default mongoose.models.Task || mongoose.model("Task", TaskSchema);
